f2302af7740a38f79a1b39f86a959c33c40ccc75
drxwat <drxwat@gmail.com>
2020-04-09 17:04:03 +0300

    backend proxy

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
 
+const apiUrl = 'https://pizza-be.herokuapp.com';
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/pizza-fe'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/pizza-fe/index.html'));
+app.use('/api', function (req, res) {
+    var url = apiUrl + req.url;
+    req.pipe(request({ qs:req.query, uri: url })).pipe(res);
+});
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/pizza-fe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
